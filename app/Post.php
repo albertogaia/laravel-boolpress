f548367df15aa2cc53e695b008af02b24284e136
albertogaia <alberto.gaia95@gmail.com>
2021-11-08 14:06:04 +0100

    Connesso il Model Post con Category

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'slug', 'thumbnail', 'author'];
+    protected $fillable = ['title', 'content', 'slug', 'thumbnail', 'author', 'category_id'];
+
+    public function category(){
+        return $this->belongsTo('App\Category');
+    }
 }
